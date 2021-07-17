276b85a3e6091a64fe7d842bdd0d121b0c306a08
Przemyslaw Kacperski <przemyslaw.kacperski@interia.pl>
2021-07-17 19:58:17 +0200

    Not working: try implementing reading pr items from file in pr-list-items-from-file

--- src/core/builder.ts
+++ src/core/builder.ts
@@ -1,7 +1,9 @@
 import { IPrItemService } from './pr-item-interface';
 import { PrItemService } from './pr-list-items';
+import { PrItemServiceFromFile } from './pr-list-items-from-file';
 
 export const createService: () => IPrItemService = () => {
-    return new PrItemService(); // or PrItemServiceFromFile() -> only 1 place to replace if therre is a need for to change! or even read that from settings file
-    // TODO: 
+    return new PrItemService(); // TODO: or PrItemServiceFromFile() -> only 1 place to replace if therre is a need for to change! or even read that from settings file
+    // return new PrItemServiceFromFile();
+    // return PrItemServiceFromFile.init();
 }
